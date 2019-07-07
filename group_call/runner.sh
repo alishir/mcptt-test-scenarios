858a9e3fc1db439ec911efc21f6cc1076ed9a916
Ali Shirvani <aj.shirvani@gmail.com>
2019-07-07 09:05:04 +0430

    add SDP to scenarios.

--- group_call/runner.sh
+++ group_call/runner.sh
@@ -9,9 +9,11 @@ sipp -t u1 -p 5090 127.0.0.1:5060 -nd -sf ./mcptt_register.xml -s mcptt_03 -m 1 
 
 sleep 2
 pkill -9 sipp
+echo "press some char ..."
+read a
 echo "Starting Callee instances ..."
-sipp -t u1 -p 5080 127.0.0.1:5060 -nd -sf ./peers.xml -s mcptt_02 -m 1 -bg -nostdin
-sipp -t u1 -p 5090 127.0.0.1:5060 -nd -sf ./peers.xml -s mcptt_03 -m 1 -bg -nostdin
+sipp -t u1 -p 5080 127.0.0.1:5060 -nd -sf ./peers.xml -mp 13300 -s mcptt_02 -m 1 -bg -nostdin
+sipp -t u1 -p 5090 127.0.0.1:5060 -nd -sf ./peers.xml -mp 13400 -s mcptt_03 -m 1 -bg -nostdin
 
 echo "Starting Caller ..."
 sipp -t u1 -p 5070 127.0.0.1:5060 -nd -sf ./caller.xml -s mcptt_01 -m 1 
